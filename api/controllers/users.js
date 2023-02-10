0bce07978b98dd8d5874fe5bdf1326e420b8bedb
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-02-10 11:52:15 +0000

    Path built to get all users

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,6 +1,18 @@
 const User = require("../models/user");
+const TokenGenerator = require("../models/token_generator");
 
 const UsersController = {
+  Index: (req, res) => {
+    User.find({}, async (err, users) => {
+      if (err) {
+        console.log('if error')
+        return res.status(400).json({error: err})
+      }
+      const token = await TokenGenerator.jsonwebtoken(req.user_id) // Added await
+      res.status(200).json({ users: users, token: token });
+    });
+  },
+
   Create: (req, res) => {
     const user = new User(req.body);
     user.save((err) => {
