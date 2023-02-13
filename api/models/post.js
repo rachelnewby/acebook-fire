1d4edea9202975e6f29ef8e417f55bb8f1e30fb0
santosluizfelipe <luizfelipe0598@icloud.com>
2023-02-13 14:52:45 +0000

    updated the schema to accept date, user_id and likes

--- api/models/post.js
+++ api/models/post.js
@@ -1,7 +1,11 @@
+const { ObjectId } = require("mongodb");
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-  message: String
+  content: { type: String, required: true },
+  date_created: { type: Date, required: true },
+  user_id: { type: ObjectId, required: true },
+  likes: { type: Number, required: true }
 });
 
 const Post = mongoose.model("Post", PostSchema);
