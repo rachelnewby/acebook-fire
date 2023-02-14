2cf2e010ec6e0547a90fc1440e92424731acd879
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-02-14 18:12:31 +0000

    comments added to potentialFriends.js

--- frontend/src/components/potentialFriends/potentialFriends.js
+++ frontend/src/components/potentialFriends/potentialFriends.js
@@ -2,6 +2,7 @@ import React from 'react';
 import jwt from 'jsonwebtoken';
 
 const PotentialFriend = ({ potentialFriend, userId }) => {
+  // Potential code to decode JWT to access user inforation
   // const payload = jwt.verify(userId, 'SUPER_SECRET')
   // console.log(userId)
   // // console.log(payload)
@@ -9,6 +10,7 @@ const PotentialFriend = ({ potentialFriend, userId }) => {
 
   return (
     <div className="potential-friend">
+      {/* ._id is unique identifier. First & surname passed through parent to child to be displayed via prop */}
       <article data-cy="post" key={ potentialFriend._id }> {potentialFriend.firstName} {potentialFriend.surname}</article>
       {/* <article key={userId}>{userId}</article> */}
       <button className="add-friend-button">Add friend</button>
