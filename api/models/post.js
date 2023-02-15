2330c3118ab55eda774d66d4e556c79bd517df02
alexHoggett <alexmh99@gmail.com>
2023-02-15 16:07:29 +0000

    updated schema

--- api/models/post.js
+++ api/models/post.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
   content: { type: String, required: true },
-  dateCreated: { type: Date, required: true },
-  userID: { type: ObjectId, required: true, ref: "User" },
+  date_created: { type: Date, required: true },
+  user_id: { type: ObjectId, required: true, ref: "User" },
   likes: [{ type: ObjectId, required: true }]
 });
 
--- api/models/user.js
+++ api/models/user.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  friendsList: [{type: Number}]
+  firstname: { type: String, required: true },
+  surname: { type: String, required: true },
+  friendsList: [{type: 'ObjectId'}]
 });
 
 const User = mongoose.model("User", UserSchema);
