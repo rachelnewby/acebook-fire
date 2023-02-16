ef02fe8a098dd433afd4dc45b24ab2c28522f742
MichalSmyk <michal.smyk39@gmail.com>
2023-02-16 16:09:03 +0000

    new working branch for comment

---
+++ frontend/src/components/comment/comment.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import { useState } from 'react';
+
+const CommentForm = ({user, post}) => {
+  const [newComment, setNewComment] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const token = window.localStorage.getItem("token");
+
+    let response = await fetch('/comments', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        user_id: user._id,
+        post_id: post._id,
+        comment: newComment
+      })
+    });
+
+    if (response.status !== 201) {
+      console.log("comment was not created");
+    } else {
+      console.log("comment was created");
+      let data = await response.json();
+      window.localStorage.setItem("token", data.token);
+    }
+  };
+
+  const handleCommentInputChange = (event) => {
+    setNewComment(event.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder='Add new comment'
+          id='comment'
+          type='text'
+          value={newComment}
+          onChange={handleCommentInputChange}
+        />
+        <button type='submit'>Add comment</button>
+      </form>
+    </div>
+  );
+};
+
+export default CommentForm;
